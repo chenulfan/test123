54fc8395a96bc05b6127d0c26dad9d3f8d5babf1
chenu <chenulfan@gmail.com>
2023-04-11 14:27:22 +0300

    as

--- server.ts
+++ server.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 4040;
 
-app.listen(PORT, () => {
-    console.log('listening on port', PORT);
-})
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("DB Connected successfully");
+    app.listen(PORT, () => console.log(`Server is running here http://localhost:${PORT}`));
+});
 
 // Remove Later
 
