0d954f7c05ca1f5da6218326b5a0e89c7276c2f2
chenu <chenulfan@gmail.com>
2023-04-11 14:44:04 +0300

    asdasdasd

--- server.ts
+++ server.ts
@@ -1,33 +1,27 @@
-import express from "express";
-import mongoose from "mongoose";
-import * as dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
 
-dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI || '');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-const PORT = process.env.PORT || 4040;
-
-mongoose.connect(process.env.MONGO_CONNECTION_STRING as string || "");
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {
-    console.log("DB Connected successfully");
-    app.listen(PORT, () => console.log(`Server is running here http://localhost:${PORT}`));
-});
-
-// Remove Later
-
-app.get('/', (req: any, res: any) => {
-    console.log("here")
-    res.send("HELLO WORLD CHECK")
+//Routes go here
+app.all('*', (req: any,res: any) => {
+    res.json({"every thing":"is awesome"})
 })
 
-
-app.get('/health', (req: any, res: any) => {
-    console.log("here")
-    res.send("health check")
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 })
-
